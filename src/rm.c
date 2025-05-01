66d6c0b662c264d70ad4cc7be17427db130821ee
andrewedx <andrew.ziadeh@gmail.com>
2025-05-01 16:18:09 +0200

    rm implementation

--- src/rm.c
+++ src/rm.c
@@ -2,31 +2,34 @@
 // Author: Maxime DAUPHIN, Andrew ZIADEH and Abbas ALDIRANI
 // Date: 2025-03-17
 
-
 #include "rm.h"
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include <errno.h>
 
 /**
  * @see fprintf
  */
-void __rm_print_usage(char *program_name) {
+void __rm_print_usage(char *program_name)
+{
     // TODO: Implement the print_usage function
     fprintf(stdout, "Usage: %s [Options]\n", program_name);
     fprintf(stdout, "Options:\n");
     fprintf(stdout, "    -h | --help    Print this help message\n");
 }
 
-
 /**
  * @see strcmp, __rm_print_usage
  */
-int __rm_parse_arguments(int argc, char *argv[]) {
+int __rm_parse_arguments(int argc, char *argv[])
+{
     // TODO: Implement the parse_arguments function
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         // Check if the argument is -h or --help
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+        {
             // Print the usage and return
             __rm_print_usage(argv[0]);
             return -1;
@@ -36,27 +39,41 @@ int __rm_parse_arguments(int argc, char *argv[]) {
     return 0;
 }
 
-
 /**
  * @see __rm_parse_arguments
  */
-int our_rm(int argc, char *argv[]) {
+int our_rm(int argc, char *argv[])
+{
     // Parse the arguments
     int parse_result = __rm_parse_arguments(argc, argv);
-    if (parse_result == -1) return 0;
-    if (parse_result)       return parse_result;
+    if (parse_result == -1)
+        return 0;
+    if (parse_result)
+        return parse_result;
 
     // TODO: Implement the rm command
-    return remove(argv[1]);
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == '-')
+        {
+            fprintf(stderr, "Warning: ignoring unsupported option '%s'\n", argv[i]);
+            continue;
+        }
+        if (remove(argv[i]) != 0)
+        {
+            fprintf(stderr, "Error removing file '%s': %s\n", argv[i], strerror(errno));
+            return -1;
+        }
+    }
 }
-
 
 #ifdef TEST_MAIN
 /**
  * ONLY FOR TESTING ALONE
  * DO NOT TOUCH
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // DO NOT TOUCH
     return our_rm(argc, argv);
 }
