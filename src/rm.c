e6dcb5ffe243608394c2e353e87eee5fb27fb4d4
andrewedx <andrew.ziadeh@gmail.com>
2025-05-02 12:02:47 +0200

    added rm flags

--- src/rm.c
+++ src/rm.c
@@ -7,6 +7,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+
+int recursive_flag;
+int verbose_flag;
+int force_flag;
 
 /**
  * @see fprintf
@@ -17,6 +25,9 @@ void __rm_print_usage(char *program_name)
     fprintf(stdout, "Usage: %s [Options]\n", program_name);
     fprintf(stdout, "Options:\n");
     fprintf(stdout, "    -h | --help    Print this help message\n");
+    fprintf(stdout, "    -r             Remove directories and their contents recursively\n");
+    fprintf(stdout, "    -v             Enable verbose mode\n");
+    fprintf(stdout, "    -f             Ignore nonexistent files and arguments, never prompt\n");
 }
 
 /**
@@ -34,8 +45,114 @@ int __rm_parse_arguments(int argc, char *argv[])
             __rm_print_usage(argv[0]);
             return -1;
         }
+
+        if (argv[i][0] == '-' && argv[i][1] != '\0')
+        {
+            // Check for options
+            for (int j = 1; argv[i][j] != '\0'; j++)
+            {
+                switch (argv[i][j])
+                {
+                case 'r':
+                    recursive_flag = 1;
+                    break;
+                case 'v':
+                    verbose_flag = 1;
+                    break;
+                case 'f':
+                    force_flag = 1;
+                    break;
+                default:
+                    fprintf(stderr, "Unknown option: -%c\n", argv[i][j]);
+                    return -1;
+                }
+            }
+        }
     }
 
+    return 0;
+}
+
+int is_directory(const char *path)
+{
+    struct stat path_stat;
+    if (stat(path, &path_stat) != 0)
+    {
+        return 0; // Assume not a directory on error
+    }
+    return S_ISDIR(path_stat.st_mode);
+}
+
+int remove_recursively(const char *path)
+{
+    DIR *dir = opendir(path);
+    if (!dir)
+    {
+        if (force_flag)
+        {
+            return 0;
+        }
+        perror("opendir");
+        return -1;
+    }
+
+    struct dirent *entry;
+    char full_path[4096];
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        // Skip the "." and ".." entries
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        if (is_directory(full_path))
+        {
+            // Recursively remove the directory
+            if (remove_recursively(full_path) != 0)
+            {
+                if (force_flag)
+                {
+                    return 0;
+                }
+                closedir(dir);
+                return -1;
+            }
+        }
+        else
+        {
+            // Remove the file
+            if (remove(full_path) != 0)
+            {
+                if (!force_flag)
+                {
+                    perror("remove");
+                    closedir(dir);
+                }
+                continue;
+            }
+            else if (verbose_flag)
+            {
+                printf("Removed file '%s'\n", full_path);
+            }
+        }
+    }
+    closedir(dir);
+    // Remove the directory itself
+    if (rmdir(path) != 0)
+    {
+        if (force_flag)
+        {
+            return 0;
+        }
+        perror("rmdir");
+        return -1;
+    }
+    if (verbose_flag)
+    {
+        printf("Removed directory '%s'\n", path);
+    }
     return 0;
 }
 
@@ -44,6 +161,9 @@ int __rm_parse_arguments(int argc, char *argv[])
  */
 int our_rm(int argc, char *argv[])
 {
+    recursive_flag = 0;
+    verbose_flag = 0;
+    force_flag = 0;
     // Parse the arguments
     int parse_result = __rm_parse_arguments(argc, argv);
     if (parse_result == -1)
@@ -56,13 +176,48 @@ int our_rm(int argc, char *argv[])
     {
         if (argv[i][0] == '-')
         {
-            fprintf(stderr, "Warning: ignoring unsupported option '%s'\n", argv[i]);
+            // fprintf(stderr, "Warning: ignoring unsupported option '%s'\n", argv[i]);
             continue;
         }
-        if (remove(argv[i]) != 0)
+        if (is_directory(argv[i]))
         {
-            fprintf(stderr, "Error removing file '%s': %s\n", argv[i], strerror(errno));
-            return -1;
+            if (recursive_flag)
+            {
+                // Remove the directory recursively
+                if (remove_recursively(argv[i]) != 0)
+                {
+                    if (force_flag)
+                    {
+                        return 0;
+                    }
+                    fprintf(stderr, "Error removing directory '%s': %s\n", argv[i], strerror(errno));
+                    return -1;
+                }
+            }
+            else
+            {
+                if (force_flag)
+                {
+                    return 0;
+                }
+                fprintf(stderr, "Error: '%s' is a directory. Use -r to remove it recursively.\n", argv[i]);
+                return -1;
+            }
+        }
+        else
+        {
+            if (remove(argv[i]) != 0)
+            {
+                if (!force_flag)
+                {
+                    fprintf(stderr, "Error removing file '%s': %s\n", argv[i], strerror(errno));
+                }
+                continue;
+            }
+            else if (verbose_flag)
+            {
+                printf("Removed file '%s'\n", argv[i]);
+            }
         }
     }
 
